42e6cfa8cbbc1307e85477270577003964d4e68a
Sydney Werbach <sydwer@gmail.com>
2020-01-08 14:00:30 -0700

    added in svelte Select component, via Yarn install. Made drop doen options for each language in system, and checked that the choices were registering to appropriate variable via a console log

--- src/pages/Create.svelte
+++ src/pages/Create.svelte
@@ -1,5 +1,66 @@
 <script>
-    import Form from '../layout/Form.svelte'
+	import Select from 'svelte-select';
+
+	const items = [
+        {value: 'Arabic', 
+        label: '🇪🇬 Arabic', 
+        group: 'Afro-asiatic'
+        },
+        {value: 'English', 
+        label: '🇬🇧 English', 
+        group: 'European'
+        },
+        {value: 'German', 
+        label: '🇩🇪 German', 
+        group: 'Eropean'
+        },
+        {value: 'Hawaiian',
+        label: '🇺🇲 Hawaiian', 
+        group: 'Polynesian'
+        },
+        {value: 'Japanese',
+        label: '🇯🇵 Japanese', 
+        group: 'Asian'
+        },
+        {value: 'Korean', 
+        label: '🇰🇷 Korean', 
+        group: 'Asian'
+        },
+        {value: 'Mandarin', 
+        label: '🇨🇳 Mandarin', 
+        group: 'Asian'
+        },
+        {value: 'Navajo', 
+        label: '🇺🇸 Navajo', 
+        group: 'Native American'
+        },
+        {value: 'Spanish', 
+        label: '🇪🇸 Spanish', 
+        group: 'European'
+        },
+        {value: 'Swahili', 
+        label: '🇹🇿 Swahili', 
+        group: 'African'
+        },
+	];
+    
+	let selectedValue = undefined;
+    let selectedGrammar = undefined;
+    
+
+    
 </script>
-<h1>We making things Over Here</h1>
-<Form></Form>
+
+<h1>Create A Language</h1>
+
+<h2>What Language's Sound System Do You Like?</h2>
+<Select {items} bind:selectedValue></Select>
+<h2>What Language's Grammar System Do You Like?</h2>
+<Select {items} bind:selectedGrammar></Select>
+<button on:click={() =>{
+        console.log(selectedValue)}}
+    >Submit</button>
+
+{#if selectedValue}
+	<p>Selected valued is: {selectedValue.label}</p>
+{/if}
