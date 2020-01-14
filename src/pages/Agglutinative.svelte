686d2896ed6ed068c809008271f1f11906ca59e1
Sydney Werbach <sydwer@gmail.com>
2020-01-14 11:04:43 -0700

    added in word generator functions for Agglutinative component, these will need to be carried in to other grammar components

--- src/pages/Agglutinative.svelte
+++ src/pages/Agglutinative.svelte
@@ -1,13 +1,33 @@
 <script>
+import { onMount } from "svelte";
 export let syllableBank
 export let mary
 export let john
 
-const test = syllableBank.length
+const test = makeBoundMorpheme(syllableBank)
+const freeMorphemeTest = makeFreeMorpheme(syllableBank)
+
+
+function makeBoundMorpheme(syllables){
+    const morpheme = syllables[Math.floor(Math.random() * syllables.length)]
+    return morpheme
+}
+
+function makeFreeMorpheme(syllables){
+    //want 2-4 syllables so 1-3, and then add one?
+    const syllableAmount = Math.floor(Math.random() * 3) + 1
+    const randomWordSyllables =[]
+    var i;
+    for(i = 0; i < syllableAmount; i ++){
+        randomWordSyllables.push(syllables[Math.floor(Math.random() * syllables.length)])
+    }
+    const joinedRandomWord = randomWordSyllables.join("")
+    return joinedRandomWord
+}
 
 
 </script>
 
 <h1>Agglutinative Grammar Component Test:</h1>
 <h2>{mary} {john} </h2>
-<h3>{test}</h3>
+<h3>{test} {freeMorphemeTest}</h3>
