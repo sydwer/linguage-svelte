cfc192902502698bede7de1f943d3450a26f125a
Sydney Werbach <sydwer@gmail.com>
2020-01-14 13:14:16 -0700

    Added in comparison page and comparisonboc component.

--- src/pages/Compare.svelte
+++ src/pages/Compare.svelte
@@ -1,2 +1,48 @@
+<script>
+    import { onMount } from "svelte";
+    import ComparisonBox from './ComparisonBox.svelte'
 
-<h1>Compare this bruh, "Ya Lame"</h1>
+    let comparisons = undefined;
+    let activeComparison = undefined;
+
+    onMount(async function() {
+        const response = await fetch("http://127.0.0.1:3000/comparisons");
+        const json = await response.json();
+        comparisons = json;
+    });
+
+    function sayHi(comparison){
+        console.log(comparison.native_language.hello)
+        activeComparison = comparison
+    }
+</script>
+<style>
+    #main{
+        display: flex;
+        flex-direction: row;
+    }
+    #comparison-list{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border: 2px solid black;
+        /* width: 25%; */
+        height: 25rem;
+        overflow: scroll;
+    }
+
+</style>
+<!-- ■ -->
+<h1>Comparisons</h1>
+<div id = "main">
+{#if comparisons}
+<div id="comparison-list">
+        {#each comparisons as comparison}
+        <h3 on:click={()=>{sayHi(comparison)}}> {comparison.native_language.name} -> {comparison.target_language.name}</h3> 
+        {/each}
+    </div>
+{/if}
+{#if activeComparison}
+<ComparisonBox comparison={activeComparison}/>
+{/if}
+</div>
---
+++ src/pages/ComparisonBox.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let comparison;
+</script>
+
+<style>
+    #main{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 75%;
+    }
+</style>
+
+<div id="main">
+<h1>{comparison.native_language.name}</h1>
+</div>
